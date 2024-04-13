b7056601ddf759f266fd1d83125c20a3658497ec
DIBAKAR DAS <careofdibakar@gmail.com>
2024-04-13 14:34:04 +0530

    Basic function declaration demonstrated

---
+++ function.js
@@ -0,0 +1,11 @@
+// function firstFunction(){
+//     return 'Normal way to declare a function'
+// }
+
+// console.log(firstFunction());
+
+const secondFunction = function secondFunction(){
+    return 'You can store function return inside a variable'
+}
+console.log(secondFunction); // [Function: secondFunction]
+console.log(secondFunction()); //You can store function return inside a variable
