7c6237459186406848a465ae083b1a873899fdae
DIBAKAR DAS <careofdibakar@gmail.com>
2024-04-14 23:33:50 +0530

    finding value of an object

---
+++ objectVolume.js
@@ -0,0 +1,12 @@
+function cube(a) {
+  // write your cube function here with parameter a
+  return a * a * a;
+}
+
+function rectangular(l, b, h) {
+  // write your rectangular function here with parameter l, b, h
+  return l * b * h;
+}
+
+console.log(cube(2));
+console.log(rectangular(2,3,4));
