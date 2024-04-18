8d5ea1fcb4981e19d763770496423f17815cdf6a
DIBAKAR DAS <careofdibakar@gmail.com>
2024-04-18 22:21:19 +0530

    anonymous function

--- function.js
+++ function.js
@@ -4,8 +4,19 @@
 
 // console.log(firstFunction());
 
-const secondFunction = function secondFunction(){
-    return 'You can store function return inside a variable'
+// const secondFunction = function secondFunction(){
+//     return 'You can store function return inside a variable'
+// }
+// console.log(secondFunction); // [Function: secondFunction]
+// console.log(secondFunction()); // You can store function return inside a variable
+
+
+// anonymus function
+
+// Function which do not have a function name that is called anonymus function
+
+let third_function = (param) => {
+    return param;
 }
-console.log(secondFunction); // [Function: secondFunction]
-console.log(secondFunction()); //You can store function return inside a variable
+
+console.log(third_function('dibakar')); 
